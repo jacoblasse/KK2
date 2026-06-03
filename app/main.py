464277a45a142749f5cd7466959beb762a1febf6
jacoblasse <jacoblasse4@gmail.com>
2026-06-03 15:36:52 +0200

    add /data/upload endpoint wiht .csv check

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, HTTPException
+from app import data
+from app.schemas import UploadResponse
+
 
 app = FastAPI(title="KK2")
 
@@ -7,3 +10,10 @@ app = FastAPI(title="KK2")
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.post("/data/upload", response_model=UploadResponse)
+async def upload_data(file: UploadFile):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="Endast .csv filer accepteras.")
+    
+    raise HTTPException(status_code=501, detail="inte implementerat än")
