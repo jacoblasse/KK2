b0b4de2484a0d965b8f3f48fe23f96e28027d66e
jacoblasse <jacoblasse4@gmail.com>
2026-06-03 23:21:00 +0200

    Add RunnableSequence and pipe operator for chan composition

--- app/chain/runnable.py
+++ app/chain/runnable.py
@@ -12,8 +12,32 @@ class Runnable(BaseModel, Generic[I, O]):
     def invoke(self, data: I) -> O:
         raise NotImplementedError("Subclasses must implement the invoke method.")
     
+    def __or__(self, other: Any) -> "RunnableSequence":
+        if isinstance(other, Runnable):
+            return RunnableSequence.model_construct(first =self, second=other)
+        if callable(other):
+            return RunnableSequence.model_construct(first =self, second=RunnableLambda(func=other))
+        return NotImplemented
+    
+    def __ror__(self, other: Any) -> "RunnableSequence":
+            if callable(other):
+                return RunnableSequence.model_construct(
+                    first=RunnableLambda.model_construct(func=other),
+                    second=self,
+                )
+            return NotImplemented
+    
 class RunnableLambda(Runnable[I, O]):
     func: Callable[[I], O]
 
     def invoke(self, data: I) -> O:
         return self.func(data)
+    
+
+
+class RunnableSequence(Runnable[I, O], Generic[I, O]):
+    first: SerializeAsAny[Runnable]
+    second: SerializeAsAny[Runnable]
+
+    def invoke(self, data: I) -> O:
+        return self.second.invoke(self.first.invoke(data))
