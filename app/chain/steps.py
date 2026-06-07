023e9e763cb126083cfc6ebd994ece62885d081a
jacoblasse <jacoblasse4@gmail.com>
2026-06-07 17:06:40 +0200

    add LLMRunner that runs the promt through SmolLM2

--- app/chain/steps.py
+++ app/chain/steps.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 from app.config import settings
 from app.chain.runnable import Runnable, RunnableLambda, RunnableSequence
+from transformers import pipeline
 
 class LLMRunnerOutput(BaseModel):
-    raw_test: str
+    raw_text: str
 
 class PromptBuilderInput(BaseModel):
     question: str
@@ -24,3 +25,14 @@ class PromptBuilder(Runnable[PromptBuilderInput, PromptBuilderOutput]):
         prompt = f"Du är en dataanalytiker som hjälper till att svara på frågor baserat på datasetets statistik. Fråga: {data.question} Stats: {data.stats_summary} Svara på frågan baserat på statistiken. "
     
         return PromptBuilderOutput(prompt=prompt)
+    
+    
+pipe = pipeline("text-generation", model=settings.llm_name)
+
+class LLMRunner(Runnable[PromptBuilderOutput, LLMRunnerOutput]):
+    def invoke(self, data: PromptBuilderOutput) -> LLMRunnerOutput:
+        messages = [
+            {"role":"user", "content":data.prompt}
+        ]
+        output = pipe(messages, max_new_tokens=500)
+        return LLMRunnerOutput(raw_text=output[0]["generated_text"][-1]["content"])
