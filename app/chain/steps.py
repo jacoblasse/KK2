7534bccedf65a4937a4e3fe276d1b84dcd77915c
jacoblasse <jacoblasse4@gmail.com>
2026-06-07 15:20:26 +0200

    implement PromptBuilder with system insruction and stats context

--- app/chain/steps.py
+++ app/chain/steps.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from app.config import settings
+from app.chain.runnable import Runnable, RunnableLambda, RunnableSequence
 
 class PromptBuilderInput(BaseModel):
     question: str
@@ -13,3 +14,10 @@ class ResponseParserOutput(BaseModel):
     prompt: str
     answer: str
     model: str = settings.llm_name
+
+
+class PromptBuilder(Runnable[PromptBuilderInput, PromptBuilderOutput]):
+    def invoke(self, data: PromptBuilderInput) -> PromptBuilderOutput:
+        prompt = f"Du är en dataanalytiker som hjälper till att svara på frågor baserat på datasetets statistik. Fråga: {data.question} Stats: {data.stats_summary} Svara på frågan baserat på statistiken. "
+    
+        return PromptBuilderOutput(prompt=prompt)
