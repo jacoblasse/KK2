51f5b6591186d1a082bad667e39b9bc9f3d99c06
jacoblasse <jacoblasse4@gmail.com>
2026-06-07 14:24:19 +0200

    add ResponseParserOutput schema

--- app/chain/steps.py
+++ app/chain/steps.py
@@ -6,3 +6,9 @@ class PromptBuilderInput(BaseModel):
 
 class PromptBuilderOutput(BaseModel):
     prompt: str
+
+
+class ResponseParserOutput(BaseModel):
+    prompt: str
+    answer: str
+    model: str = "HuggingFaceTB/SmolLM2-135M-Instruct"
